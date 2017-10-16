a13f1689f01e53dd80c325d92822965ca42013c6
Łukasz Rosik <lukiro96@gmail.com>
2017-10-16 20:22:44 +0200

    Changed function to return list or string, not to print output in function

--- app.py
+++ app.py
@@ -4,23 +4,28 @@ from difflib import get_close_matches
 
 def translate(word, dict):
     if word in dict:
-        for value in dict[word]:
-            print(f"- {value}")
+        return dict[word]
     elif get_close_matches(key, dict.keys(), cutoff=0.75):
         possibleKeys = get_close_matches(key, data.keys(), cutoff=0.75)
         yesNo = input(f"Did You mean {possibleKeys[0]}? yes/no\n").lower()
         if yesNo == 'yes' or yesNo == 'y':
-            for value in dict[possibleKeys[0]]:
-                print(f"- {value}")
-        elif:
-            print("The word doesn't exist. Please try again.")
+            return dict[possibleKeys[0]]
+        elif yesNo == 'no' or yesNo == 'n':
+            return "The word doesn't exist. Please try again."
         else:
-            print("Incorrect input.")
+            return "Incorrect input."
     else:
-        print("The word doesn't exist. Please try again.")
+        return "The word doesn't exist. Please try again."
 
 
 data = json.load(open('data.json'))
 
 key = input("Enter key you want to find.\n").lower()
-translate(key, data)
+
+output = translate(key, data)
+
+if type(output) is list:
+    for value in output:
+        print(f"- {value}")
+else:
+    print(output)
