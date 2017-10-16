f6dfe4064203acd0b2eb4adccdded4566412466d
Łukasz Rosik <lukiro96@gmail.com>
2017-10-16 14:18:11 +0200

    IF statements instead of try/except
    - using if statements intesad of try/except
    - corrected printing of definitions to look better

--- app.py
+++ app.py
@@ -4,20 +4,19 @@ from difflib import get_close_matches
 
 
 def translate(word, dict):
-    return dict[word]
+    if word in dict:
+        for value in dict[word]:
+            print(f"- {value}")
+    elif get_close_matches(key, dict.keys(), cutoff=0.75):
+        possibleKeys = get_close_matches(key, data.keys(), cutoff=0.75)
+        print(f"Did You mean {possibleKeys[0]}?")
+        for value in dict[possibleKeys[0]]:
+            print(f"- {value}")
+    else:
+        print("The word doesn't exist. Please try again.")
+
 
 data = json.load(open('data.json'))
 
-while True:
-    try:
-        key = input("Enter key you want to find.\n").lower()
-        print(translate(key, data))
-        break
-    except KeyError:
-        possibleKeys = get_close_matches(key, data.keys())
-        if not possibleKeys:
-            print("The word doesn't exist. Please try again.")
-        else:
-            print(f"The word doesn't exist. Did You mean {possibleKeys[0]}?")
-            print(translate(possibleKeys[0], data))
-            break
+key = input("Enter key you want to find.\n").lower()
+translate(key, data)
